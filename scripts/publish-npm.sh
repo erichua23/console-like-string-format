21a5dd0e574d6ac12dc8f4a53fa252b9b47dc697
erichua <erichua@webank.com>
2017-06-12 03:22:19 +0800

    add dependencies

--- scripts/publish-npm.sh
+++ scripts/publish-npm.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
+
 # if [ $# -lt 1 ];then
 # echo 'Your should input your version numver'
 # exit
 # fi
-
-git add -A
-git commit
-
+version=`npm version patch`
 cp -r README.md ./lib
-version=v$1
 git tag $version
 npm publish --access public --verbose
 git push --tag
